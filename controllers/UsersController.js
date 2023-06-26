44e317e806483f1d7c640c7125d3614b99cf0507
Ian Kisali <iankisali@gmail.com>
2023-06-26 21:49:32 +0300

    add users to our database

---
+++ controllers/UsersController.js
@@ -0,0 +1,23 @@
+import UtilController from './UtilController';
+import dbClient from '../utils/db';
+
+export default class UsersController {
+    static async postNew(req, res) {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            res.status(400).json({ error: `Missing ${email ? 'email' : 'password'}` }).end();
+        } else if (await dbClient.userExists(email)) {
+                res.status(400).json({ error: 'Already exist' }).end();
+            } else {
+                try {
+                    const hash = UtilController.SHA1(password);
+                    const insert = await dbClient.newUser(email, hash);
+                    const { _id } = insert.ops[0];
+                    const _email = insert.ops[0].email;
+                    res.status(201).json({ id: _id, email: _email }).end();
+                } catch (err) {
+                    res.status(400).json({ error: err.message }).end();
+                }
+            }
+        }
+    }
