99eddd8d2c7cb0d2ea93f5e5a38ddc39fba6ceba
Terer <terermike3@gmail.com>
2023-06-28 22:17:35 +0000

    index.js

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,28 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+import UtilController from '../controllers/UtilController';
 
 const router = Router();
 
-router.use((req, res, next) => {
-  const path = ['/connect'];
-  if (!path.includes(req.path)) {
+router.use((request, response, next) => {
+  const paths = ['/connect'];
+  if (!paths.includes(request.path)) {
     next();
-  } else if (!req.headers.authorization) {
-    res.status(401).json({ error: 'Unauthorized' }).end();
+  } else if (!request.headers.authorization) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
   } else {
     next();
   }
 });
 
-router.use((req, res, next) => {
-  const path = ['disconnect', '/users/me', '/files'];
-  if (!path.includes(req.path)) {
+router.use((request, response, next) => {
+  const paths = ['/disconnect', '/users/me', '/files'];
+  if (!paths.includes(request.path)) {
     next();
-  } else if (!req.headers['x-token']) {
-    res.status(401).json({ error: 'Unauthorized' }).end();
+  } else if (!request.headers['x-token']) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
   } else {
     next();
   }
@@ -33,6 +35,10 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
+router.put('/files/:id/publish', UtilController.token, FilesController.putPublish);
+router.put('/files/:id/unpublish', UtilController.token, FilesController.putUnpublish);
 module.exports = router;
